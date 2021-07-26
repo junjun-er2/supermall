11bb4f65a7fad33014d2f6e346a8beeb56f22359
junjun-er2 <9200511+junjun-er2@user.noreply.gitee.com>
2021-07-26 18:36:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <keep-alive exclude="Detail">
+      <router-view/>
+    </keep-alive>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+import MainTabBar from "components/content/mainTabbar/MainTabBar"
+export default {
+  name: 'App',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
+<style>
+@import "assets/css/base.css";
+body {
+  max-width: 450px;
+  margin: 0 auto;
+}
+li{
+  list-style: none;
+}
+
+</style>
